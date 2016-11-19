1622091d96e2403a4304fce02056fae8a05c1405
Adil BOUGAMZA <Adil.Bougamza@nespresso.com>
2016-11-19 12:23:59 +0000

    Initial Commit

---
+++ UsabillaDashboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UsabillaDashboard
+//
+//  Created by Adil BOUGAMZA on 19/11/2016.
+//  Copyright © 2016 Adil Bougamza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UsabillaDashboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UsabillaDashboard
+//
+//  Created by Adil BOUGAMZA on 19/11/2016.
+//  Copyright © 2016 Adil Bougamza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
