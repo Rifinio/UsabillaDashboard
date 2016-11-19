7801a62bba69876d306b8c012e3bbe6c17f07927
Adil BOUGAMZA <Adil.Bougamza@nespresso.com>
2016-11-19 13:12:49 +0000

    Create DashboardViewModel

---
+++ UsabillaDashboard/ViewModels/DashboardViewModel.h
@@ -0,0 +1,13 @@
+//
+//  DashboardViewModel.h
+//  UsabillaDashboard
+//
+//  Created by Adil BOUGAMZA on 19/11/2016.
+//  Copyright © 2016 Adil Bougamza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DashboardViewModel : NSObject
+
+@end
---
+++ UsabillaDashboard/ViewModels/DashboardViewModel.m
@@ -0,0 +1,13 @@
+//
+//  DashboardViewModel.m
+//  UsabillaDashboard
+//
+//  Created by Adil BOUGAMZA on 19/11/2016.
+//  Copyright © 2016 Adil Bougamza. All rights reserved.
+//
+
+#import "DashboardViewModel.h"
+
+@implementation DashboardViewModel
+
+@end
