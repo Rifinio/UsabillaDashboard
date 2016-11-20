05edc6da75ac1978fc49a46335926cc355ed4f0a
Adil BOUGAMZA <Adil.Bougamza@nespresso.com>
2016-11-20 16:40:44 +0000

    Create DetailviewController

---
+++ UsabillaDashboard/Views/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  UsabillaDashboard
+//
+//  Created by Adil BOUGAMZA on 20/11/2016.
+//  Copyright © 2016 Adil Bougamza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ UsabillaDashboard/Views/DetailViewController.m
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.m
+//  UsabillaDashboard
+//
+//  Created by Adil BOUGAMZA on 20/11/2016.
+//  Copyright © 2016 Adil Bougamza. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor redColor];
+    self.navigationItem.title = @"Brrowsers";
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
