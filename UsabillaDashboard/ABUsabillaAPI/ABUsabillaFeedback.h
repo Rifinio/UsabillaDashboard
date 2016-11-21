226c994bd349cef2942b5ad73672500638fc3069
Adil BOUGAMZA <Adil.Bougamza@nespresso.com>
2016-11-21 01:04:47 +0000

    Create ABUsabillaFeedbackViewController for feedback

---
+++ UsabillaDashboard/ABUsabillaAPI/ABUsabillaFeedback.h
@@ -0,0 +1,25 @@
+//
+//  ABUsabillaFeedback.h
+//  UsabillaDashboard
+//
+//  Created by Adil BOUGAMZA on 20/11/2016.
+//  Copyright © 2016 Adil Bougamza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol ABUsabillaFeedbackDelegate <NSObject>
+
+@end
+
+@interface ABUsabillaFeedback : NSObject
+
+@property (nonatomic, weak) id delegate;
+@property (nonatomic, strong) NSArray<Class> *classes;
+@property (nonatomic, assign) BOOL isFeedBackModeActivated;
+
+- (void)highlightViews;
++ (UIImage *)usabillaBarButtonImage;
+
+@end
---
+++ UsabillaDashboard/ABUsabillaAPI/ABUsabillaFeedbackViewController.m
@@ -0,0 +1,30 @@
+//
+//  ABUsabillaFeedbackViewController.m
+//  UsabillaDashboard
+//
+//  Created by Adil BOUGAMZA on 21/11/2016.
+//  Copyright © 2016 Adil Bougamza. All rights reserved.
+//
+
+#import "ABUsabillaFeedbackViewController.h"
+#import <Masonry/Masonry.h>
+
+@interface ABUsabillaFeedbackViewController ()
+
+@end
+
+@implementation ABUsabillaFeedbackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
