8aac5fa05dc1dd0c5e371346af760a2d83575c48
Adil BOUGAMZA <Adil.Bougamza@nespresso.com>
2016-11-22 13:58:02 +0000

    Kiwi test Feedback Tests

--- UsabillaDashboardTests/FeedbackTests.m
+++ UsabillaDashboardTests/FeedbackTests.m
@@ -20,29 +20,33 @@ static NSInteger dummyRating = 5;
 SPEC_BEGIN(FeedbackTests)
 
 describe(@"Feedback", ^{
-
-    let(feedBackJson, ^{
+    context(@"When Feedback object is initilized", ^{
         NSDictionary *feedBackDict = @{@"rating": @(dummyRating),
                                        @"labels":@[dummyLabel],
                                        @"email":dummyEmail,
                                        @"computed_location":dummyLocation,
-                                       @"computed_browser":@{@"Browser":dummyBorwser},
-                                       @"computed_browser":@{@"Platform":dummyLocation}
+                                       @"computed_browser":@{@"Browser":dummyBorwser, @"Platform":dummyPlatform},
                                        };
-        return feedBackDict;
-    });
 
-
-    context(@"When Feedback object is initilized", ^{
-        Feedback *feedback = [[Feedback alloc] initWithJsonDict:feedBackJson];
+        Feedback *feedback = [[Feedback alloc] initWithJsonDict:feedBackDict];
 
         it(@"should not be nil", ^{
             [[feedback shouldNot] beNil];
         });
 
+        it(@"All values should match", ^{
+            [[feedback.email should] equal:dummyEmail];
+            [[feedback.geoLocation should] equal:dummyLocation];
+            [[feedback.browserName should] equal:dummyBorwser];
+            [[feedback.platform should] equal:dummyPlatform];
+            [[theValue(feedback.rating) should] equal:@(dummyRating)];
+            [[theValue(feedback.labels.count == @[dummyLabel].count) should] beTrue];
+            [[feedback.labels should] containObjectsInArray:@[dummyLabel]];
+        });
+
     });
-
-
+    
+    
 });
 
 SPEC_END
